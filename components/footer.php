2b3a5fd3c3ef8415160da84aa2e956d66372e7ae
nirajs0 <nirajsoofficial@gmail.com>
2025-04-29 01:04:13 +0530

    Final Commit

---
+++ components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact us</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +91 9171955834</a>
+         <a href="tel:11122233333"><i class="fas fa-phone"></i> +91 7987368301</a>
+         <a href="mailto:shaikh@gmail.com"><i class="fas fa-envelope"></i> nirajsoni618@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Betul, india - 460001 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/profile.php?id=100036582525888&mibextid=ZbWKwL"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/Sahilga54687386?t=KFYCfQLfjS7BICNkT-keyg&s=09"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="https://instagram.com/_sahil_gadekar?igshid=ZDdkNTZiNTM="><i class="fab fa-instagram"></i>instagram</a>
+         <a href="https://www.linkedin.com/in/niraj-soni-764498272"><i class="fab fa-linkedin"></i>linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span><i>Mr.Niraj & Sahil</i></span> | all rights reserved!</div>
+
+</footer>
